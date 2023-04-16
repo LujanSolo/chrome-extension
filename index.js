951dec1f619310edfc813e4e3b91d5622753ce05
LujanSolo <lujansolo@proton.me>
2023-04-16 12:58:49 -0700

    added for loop to push leads to console

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const buttonEl = document.getElementById('input-btn');
 const searchEl = document.getElementById('input-el');
 
-let myLeads = [];
+let myLeads = [
+    "test1",
+    "test2",
+    "test3"
+];
 
 
 buttonEl.addEventListener('click', () => {
     myLeads.push(searchEl.value);
-    console.log(myLeads);
 });
+
+for(let i=0; i < myLeads.length; i++){
+    console.log(myLeads[i])
+}
