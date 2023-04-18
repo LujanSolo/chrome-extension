f653c285f503f716bea097f5eee3ef7720fdfef3
LujanSolo <lujansolo@proton.me>
2023-04-17 21:35:32 -0700

    changed ol to a ul on the html; added properties to begin styling the ul

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function renderSearch() {
   for (let i = 0; i < myLeads.length; i++) {
     // listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>";
     listItems += `<li>
-                      <a href='${myLeads[i]}' target="_blank">
-                        ${myLeads[i]}
-                      </a>
-                    </li>
-                    `
+                    <a href='${myLeads[i]}' target="_blank">
+                      ${myLeads[i]}
+                    </a>
+                  </li>
+                  `
   }
   ulEl.innerHTML = listItems;
 };
