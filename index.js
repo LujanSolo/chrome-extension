c8a812b860563ec9a9193f7a0cb34df8ce6857b4
LujanSolo <lujansolo@proton.me>
2023-04-19 16:07:03 -0700

    added detailed notes to index.js

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
+// variables for html elements
 const buttonEl = document.getElementById("input-btn");
 const searchEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 
+// array to hold existing websites
 let myLeads = [];
 
+// variable to parse JSON to a string
 let storedLeads = JSON.parse(localStorage.getItem("myLeads"));
-console.log(storedLeads);
 
+
+// button function that pushes user's entry from input field to the myLeads array
 buttonEl.addEventListener("click", () => {
   myLeads.push(searchEl.value);
-  searchEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderSearch();
+  searchEl.value = ""; // reset input field to blank
+  localStorage.setItem("myLeads", JSON.stringify(myLeads)); // store searches to window.localStorage
+  renderSearch(); // call renderSearch function
 });
 
+// renderSearch function that dynamically creates li's within the declared ul on index.html
 function renderSearch() {
-  let listItems = "";
+  let listItems = "";  // variable to hold all list items inside a string
   for (let i = 0; i < myLeads.length; i++) {
-    // listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>";
+    // template string holding list elements and js references to myLeads array
     listItems += `<li>
                     <a href='${myLeads[i]}' target="_blank">
                       ${myLeads[i]}
@@ -25,5 +30,6 @@ function renderSearch() {
                   </li>
                   `
   }
-  ulEl.innerHTML = listItems;
+  //* note: putting this part of the function outside of the for loop allows us to manipulate the DOM once instead of several times (depending on # of list items that are created)
+  ulEl.innerHTML = listItems; // take all created li's and add them as children to the ul
 };
